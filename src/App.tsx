83ce6fdb533648b3287faf2bd375992cdad0046f
ditiz <mathieu.smarzyk@campus-numerique-in-the-alps.com>
2019-07-11 05:58:24 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.scss";
+
+interface Item {
+  id: number;
+  name: string;
+  content: string;
+  date: Date;
+}
 
 const App: React.FC = () => {
+  const [items, setItems] = useState<Array<Item>>([
+    {
+      id: 0,
+      name: "pouet",
+      content: "first article",
+      date: new Date()
+    },
+    {
+      id: 1,
+      name: "test",
+      content: "seconde article who don't have much to say",
+      date: new Date()
+    }
+  ]);
+
+  const handleClick = () => {
+    const newItem: Item = {
+      id: items.length,
+      name: `New item ${items.length}`,
+      content: "This is a new item juste create",
+      date: new Date()
+    };
+    setItems([...items.sort((a: Item, b: Item) => a.id - b.id), newItem]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header>
+        <h1>Dev-Log</h1>
+        <button onClick={handleClick}>Add</button>
       </header>
+      <main>
+        {items.reverse().map((item: Item) => (
+          <article className="item">
+            <h2>{item.name}</h2>
+            <div>{item.content}</div>
+          </article>
+        ))}
+      </main>
     </div>
   );
-}
+};
 
 export default App;
